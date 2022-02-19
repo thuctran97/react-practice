c47c49d3048c28c513b6998a4563801c8a341d96
Thuc Tran <congthuc.tran@navercorp.com>
2022-02-19 15:58:26 +0700

    add list

--- src/index.js
+++ src/index.js
@@ -11,28 +11,42 @@ import ReactDOM from "react-dom";
 //always capitalize name
 //use camelCase for HTML attr: className
 
-
+const books = [
+  {
+    title: "harry potter",
+    author: "jk rowling",
+  },
+  {
+    title: "im good",
+    author: "adam khoo",
+  },
+];
 // Create function, return jsx
 function BookList() {
   const person_name = "thuc";
   return (
     <div>
-      <Book job = 'developer'/>
-      <Book title = 'harry potter'/>
+      {/* <Book job = 'developer'/>
+      <Book title = 'harry potter'/> */}
+      {books.map((book, index) => {
+        // console.log(book.title);
+        return <Book key={index} book={book}></Book>;
+      })}
     </div>
   );
 }
 //Using props as paramemeters
 const Book = (props) => {
-  const author = "jk";
-  const {job, title} = props; //props destructuring
+  const publish_year = "2000";
+  const { job, title } = props.book; //props destructuring
   return (
     <div>
       <br />
       <h5>{props.job}</h5>
-      <h5>{author.toUpperCase()}</h5>
+      {/* <h5>{author.toUpperCase()}</h5> */}
       <h1>{job}</h1>
       <h1>{title}</h1>
+      <h1>{publish_year}</h1>
     </div>
   );
 };
